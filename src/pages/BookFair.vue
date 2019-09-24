7634877a318668abe7d0d6572c429f5b693de59e
Aidan Dennehy <duinneacha@gmail.com>
2019-09-24 21:32:39 +0100

    Front page work

--- src/pages/BookFair.vue
+++ src/pages/BookFair.vue
@@ -5,16 +5,77 @@
     <div class="q-pa-md">
     <q-card class="my-card">
       <q-parallax
-        src="https://cdn.quasar.dev/img/parallax1.jpg"
-        :height="150"
+        src="https://panoramicireland.com/images/osservicesbooking/services/1513430931_panoramic-ireland-New-York-Times-36-Hours-Cork.jpg"
+        
+        :height="200"
       />
 
       <q-card-section>
-        <div class="text-h6 center">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
+        <div class="text-h2 text-center">Introduction</div>
+        <div class="text-subtitle2">In Cork City Hall and Pairc Ui Chaoimh 18th-22nd July </div>
         <div class="row">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deleniti, quae beatae distinctio, quia nemo iste modi tempora, voluptatem sint ut veritatis est minima. Veniam ipsa quae minima. Perferendis totam adipisci quasi, magnam quo qui quis deserunt eum? Fugiat incidunt qui veniam et dolorum aliquid impedit iste sapiente ab porro officia, hic a eveniet perferendis saepe ea culpa provident quos nam vitae reprehenderit. Repellat magnam in ea deleniti commodi architecto, deserunt sunt cumque quod optio, ad sequi beatae recusandae enim repellendus eaque nostrum obcaecati error! Veritatis aliquam voluptates quibusdam est ipsam at omnis nesciunt numquam magni dignissimos? Maxime, totam accusantium! Dolores, cumque fugit officia quo, odit ipsum consequatur amet ducimus id atque quod maxime architecto quos eum aliquid sed aut voluptate obcaecati, laudantium perferendis inventore nostrum praesentium quisquam. Nesciunt natus eum fugit velit commodi earum dignissimos atque, quae inventore illo vel quibusdam porro sapiente autem ducimus nihil qui eligendi, reiciendis, alias repellat praesentium esse numquam itaque? Reprehenderit laborum, asperiores officia ea laboriosam suscipit? Nihil, ipsa? Dolores nisi, unde sint hic dignissimos at incidunt ipsa quaerat maiores officia adipisci odio perspiciatis enim reiciendis nobis esse! Aut repellendus rem nisi perspiciatis officia minima soluta, optio odio ratione cum temporibus saepe ipsa explicabo vel reiciendis quasi, adipisci neque velit ad autem. Tempore iste accusamus qui est quis eligendi perspiciatis, inventore libero non vero labore veniam molestias corporis, dolores exercitationem, fugiat quidem quae? Velit possimus magnam itaque quisquam perspiciatis exercitationem reprehenderit quidem alias maiores dolorem commodi error ipsum totam eius, unde, inventore natus repudiandae. Ut ea ullam unde cupiditate a amet quas commodi fuga molestias nihil laboriosam quia iure autem qui necessitatibus, sapiente distinctio modi possimus ducimus suscipit. Exercitationem deleniti veritatis hic voluptate ex iure! Beatae voluptatibus corrupti ab libero recusandae. Culpa provident error libero illum, ea hic corporis, possimus consequuntur maiores necessitatibus mollitia corrupti impedit? Doloribus suscipit ipsa, accusantium deserunt eveniet repellendus quasi neque omnis illo ipsam animi, fuga est nihil corporis eius odio aliquid quis vero? Cupiditate, aut! Labore atque, placeat tempora perspiciatis rerum quod corporis voluptatem rem ab quas officiis iusto fuga dolore eum voluptates. Reiciendis quaerat nostrum aspernatur excepturi, quam, blanditiis amet et qui repellendus fuga iusto est officia sapiente alias quidem magni voluptatum quis? Consequatur, quasi! Deleniti ipsum placeat totam mollitia sequi excepturi velit ipsam, esse error beatae dignissimos ratione? Eos, veniam facere dolores tempore nesciunt, quam ullam velit, odio quo asperiores exercitationem numquam laboriosam ratione sunt! Temporibus blanditiis commodi dolores, animi sed magni nihil ex quam perspiciatis saepe repellendus eum sapiente dolor quas quia similique veniam qui beatae vero nulla dignissimos ea fugiat quae? Saepe tempora consectetur tempore omnis, laborum culpa dolor natus sunt laboriosam, molestiae, ut sit rerum dignissimos? Soluta incidunt sed magnam impedit eveniet molestias ratione architecto quisquam provident. Laboriosam consequuntur quo officia, harum corporis ratione ex hic adipisci dolore, non unde placeat ullam voluptate, minima mollitia quasi provident illum excepturi quos quibusdam expedita? Veritatis dolor ea iusto asperiores modi et, quasi atque, velit earum nihil ducimus maxime ratione blanditiis nesciunt. Officia quidem dicta odio maiores!</p>
+          <p> {{ lorem }} </p>
         </div>
+      </q-card-section>
+    </q-card>
+  </div>
+   <div class="q-pa-md">
+    <q-carousel
+      swipeable
+      animated
+      arrows
+      v-model="slide"
+      :fullscreen.sync="fullscreen"
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <q-carousel-slide :name="5" img-src="./statics/adimgs/Adam Wyeth_Colour.jpg" />
+
+      <template v-slot:control>
+        <q-carousel-control
+          position="bottom-right"
+          :offset="[18, 18]"
+        >
+          <q-btn
+            push round dense color="white" text-color="primary"
+            :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+            @click="fullscreen = !fullscreen"
+          />
+        </q-carousel-control>
+      </template>
+    </q-carousel>
+  </div>
+
+  <div class="row q-pa-md text-h3 text-center">Renowned Writers</div>
+  <div class="q-pa-md row items-start q-gutter-md">
+    
+    <!-- <q-img
+      v-for="author in authors"
+      :key="author.id"
+      :src="author.image"
+      spinner-color="white"
+      style="height: 140px; max-width: 150px"
+    /> -->
+
+     <q-card
+       class="author-card"
+       v-for="author in authors"
+       :key="author.id"
+       >
+
+      <q-img
+         :src="author.image"/>
+
+      <q-card-section>
+        <div class="text-h6">{{ author.name }}</div>
+        <div class="text-subtitle2">{{ author.country }}</div>
+      </q-card-section>
+
+      <q-card-section>
+        {{ lorem }}
       </q-card-section>
     </q-card>
   </div>
@@ -26,7 +87,97 @@
 export default {
   data() {
     return {
-      introMessage: 'Cork Book Fair Introduction'
+      introMessage: 'Cork Book Fair Introduction',
+      authors: [
+        {
+          id: 1,
+          name: 'Adam Wyeth',
+          country: 'England',
+          image: './statics/adimgs/Adam Wyeth_Colour.jpg'
+        },
+        {
+          id: 2,
+          name: 'Alice Kinsella',
+          country: 'Dublin',
+          image: './statics/adimgs/Alice_Kinsella.jpg'
+        },
+        {
+          id: 3,
+          name: 'Bill Whelan',
+          country: 'Cork',
+          image: './statics/adimgs/Bill-Whelan.jpg'
+        },
+        {
+          id: 4,
+          name: 'Cecilia Ahern',
+          country: 'Kildare',
+          image: './statics/adimgs/Cecilia-Ahern.jpg'
+        },
+        {
+          id: 5,
+          name: 'Christopher Lloyd',
+          country: 'England',
+          image: './statics/adimgs/Christopher-Lloyd.jpg'
+        },
+        {
+          id: 6,
+          name: 'Cinta Ramblado',
+          country: 'Portugal',
+          image: './statics/adimgs/Cinta-Ramblado.jpg'
+        },
+        {
+          id: 7,
+          name: 'Clodagh Beresford-Dunne',
+          country: 'Wales',
+          image: './statics/adimgs/Clodagh-Beresford-Dunne_Colour.jpg'
+        },
+        {
+          id: 8,
+          name: 'Donal Ryan',
+          country: 'Limerick',
+          image: './statics/adimgs/Donal-Ryan.jpg'
+        },
+        {
+          id: 9,
+          name: 'Emer McLysaght',
+          country: 'Edinburgh',
+          image: './statics/adimgs/Emer-McLysaght.jpg'
+        },
+        {
+          id: 10,
+          name: 'Emilie Pine',
+          country: 'France',
+          image: './statics/adimgs/Emilie Pine_Colour.jpg'
+        },
+        {
+          id: 11,
+          name: 'Eoin Devereux',
+          country: 'Switzerland',
+          image: './statics/adimgs/Eoin-Devereux.jpg'
+        },
+        {
+          id: 12,
+          name: 'Jo Slade',
+          country: 'Dublin',
+          image: './statics/adimgs/Jo Slade_Colour.jpg'
+        },
+        {
+          id: 13,
+          name: 'Loic Guyon',
+          country: 'Belgium',
+          image: './statics/adimgs/Loic_Guyon_Colour.jpg'
+        },
+        {
+          id: 14,
+          name: 'Louise ONeill',
+          country: 'Tipperary',
+          image: './statics/adimgs/Louise ONeill_Colour.jpg'
+        }
+      ],
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      slide: 1,
+      fullscreen: false
+
     }
   }
 }
@@ -36,5 +187,9 @@ export default {
 .my-card {
   width: 100%;
   
+}
+.author-card {
+  width: 100%;
+  max-width: 250px;
 }
 </style>
--- src/pages/corkbookfair.vue
+++ src/pages/corkbookfair.vue
@@ -10,7 +10,79 @@
 export default {
   data() {
     return {
-      homeMessage: 'Cork Book Fair'
+      homeMessage: 'Cork Book Fair',
+      authors: [
+        {
+          id: 1,
+          name: 'Adam Wyeth',
+          imgage: 'https://imgur.com/0Y12tF8'
+        },
+        {
+          id: 2,
+          name: 'Alice Kinsella',
+          imgage: 'https://imgur.com/3kwdjOq'
+        },
+        {
+          id: 3,
+          name: 'Bill Whelan',
+          imgage: 'https://imgur.com/Ax7NWKc'
+        },
+        {
+          id: 4,
+          name: 'Cecilia Ahern',
+          imgage: 'https://imgur.com/BWbTE89'
+        },
+        {
+          id: 5,
+          name: 'Christopher Lloyd',
+          imgage: 'https://imgur.com/21hoJ4s'
+        },
+        {
+          id: 6,
+          name: 'Cinta Ramblado',
+          imgage: 'https://imgur.com/5ChFgsJ'
+        },
+        {
+          id: 7,
+          name: 'Clodagh Beresford-Dunne',
+          imgage: 'https://imgur.com/QKwYidG'
+        },
+        {
+          id: 8,
+          name: 'Donal Ryan',
+          imgage: 'https://imgur.com/D9wvAQ9'
+        },
+        {
+          id: 9,
+          name: 'Emer McLysaght',
+          imgage: 'https://imgur.com/YmtDEJy'
+        },
+        {
+          id: 10,
+          name: 'Emilie Pine',
+          imgage: 'https://imgur.com/VGRBusI'
+        },
+        {
+          id: 11,
+          name: 'Eoin Devereux',
+          imgage: 'https://imgur.com/hCzqzdi'
+        },
+        {
+          id: 12,
+          name: 'Jo Slade',
+          imgage: 'https://imgur.com/hDFC2bF'
+        },
+        {
+          id: 13,
+          name: 'Loic Guyon',
+          imgage: 'https://imgur.com/geVMeE1'
+        },
+        {
+          id: 14,
+          name: 'Louise ONeill',
+          imgage: 'https://imgur.com/htXAR3v'
+        }
+      ]
     }
   }
 }
