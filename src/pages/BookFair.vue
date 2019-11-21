4a2d2e41cecf056c7027135528f5942e8b4c46ba
Aidan Dennehy <duinneacha@gmail.com>
2019-11-21 21:02:57 +0000

    Added Scroll to Top

--- src/pages/BookFair.vue
+++ src/pages/BookFair.vue
@@ -116,7 +116,10 @@
       </q-card-section>
     </q-card>
   </div>
-  
+  <!-- place QPageScroller at end of page -->
+  <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+    <q-btn fab icon="keyboard_arrow_up" color="accent" />
+  </q-page-scroller>
   </q-page>
 </template>
 
